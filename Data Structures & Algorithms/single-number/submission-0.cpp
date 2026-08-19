b02beb0322d4eca45ae635d9c952bb77cbb6d7a8
Satyam Vishwakarma <157634304+sat-sde@users.noreply.github.com>
2026-08-19 18:18:10 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(auto it:nums){
+            ans^=it;
+        }
+        return ans;
+    }
+};
